979703ed18ecd2b9b81179749257bd1cdab591d3
Source <Limit@outlook.com>
2023-03-11 14:35:57 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,1 @@
-from flask import Flask, render_template, request
-
-app = Flask(__name__)
-
-# Define a route for the home page
-@app.route('/')
-def home():
-    return render_template('home.html')
-
-# Define a route for the form submission
-@app.route('/post_cves', methods=['POST'])
-def post_cves():
-    cves = request.json.get('cves', [])
-    # Code to process the CVE information and display it on the website
-    return "CVE information posted successfully"
-
-if __name__ == '__main__':
-    app.run()
+- removed 
